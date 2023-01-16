ec5c0911c5dd5e2d37984224fd078a86308e8681
Jessie <jessica.v.rid@gmail.com>
2023-01-16 23:05:13 +0300

    Task 5 is solved

---
+++ task5_3d_symbol/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("enter 3-digit number");
+int N = Convert.ToInt32(Console.ReadLine());
+if(99 < N && N < 1000) // && and когда 2е условие собл и то и другое  || or или так или так 
+{
+    System.Console.WriteLine(N % 10);
+}
+else System.Console.WriteLine("incorrect number");
