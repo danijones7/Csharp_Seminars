b00ad77387ea3ae8badaeec225fde628291afb5b
Jessie <jessica.v.rid@gmail.com>
2023-01-16 20:57:03 +0300

    Task 1 is solved

---
+++ task1/Program.cs
@@ -0,0 +1,9 @@
+﻿// dotnet - пролверка все ли установлено 
+// dotnet new console - создание проекта
+// dotnet run - запуск проекта
+
+Console.Write("enter integer number: "); // Вываод в консоль сообщения 
+int number = Convert.ToInt32(Console.ReadLine()); // int.Parse  Convert.ToInt32() помогает в строку записать число, а не текст, предпочтительнее исп-ть Convert
+int sqr = number * number;
+System.Console.WriteLine($"Square number:  {sqr}"); //значок $ запускает форматирование, мы в тексте в фигурных скобках можем вставить переменную, иначе будет 
+//System.Console.WriteLine("some text:" + sqr + "some more text");
