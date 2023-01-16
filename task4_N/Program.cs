5986fd413aaf8cd94980c759bbf3bf44625499a8
Jessie <jessica.v.rid@gmail.com>
2023-01-16 22:45:00 +0300

    Task 4 num N solved

---
+++ task4_N/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("enter number");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = N * -1;
+while (M < N) // или (M != N) не равно 
+{
+    System.Console.WriteLine(M);
+    M ++;
+}
