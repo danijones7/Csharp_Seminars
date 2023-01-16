65e53f24acb835547793736c00d93370f27127da
Jessie <jessica.v.rid@gmail.com>
2023-01-16 21:43:06 +0300

    task3 days is  solved

---
+++ task3_days/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine("enter number");
+int daynumber = Convert.ToInt32(Console.ReadLine());
+if (daynumber == 1)
+{
+System.Console.WriteLine("Monday");
+}
+else if (daynumber == 2)
+{
+System.Console.WriteLine("Tuesday");
+}
+else if (daynumber == 3)
+{
+System.Console.WriteLine("Wednesday");
+}
+else if (daynumber == 4)
+{
+System.Console.WriteLine("Thursday");
+}
+else if (daynumber == 5)
+{
+System.Console.WriteLine("Friday");
+}
+else if (daynumber == 6)
+{
+System.Console.WriteLine("Saturday");
+}
+else if (daynumber == 7)
+{
+System.Console.WriteLine("Sunday");
+}
+else System.Console.WriteLine("There is no such day");
