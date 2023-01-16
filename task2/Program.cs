2c8712cba732569c256087b259ea1ef0367bfc40
Jessie <jessica.v.rid@gmail.com>
2023-01-16 21:27:14 +0300

    task2 solved an modified

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("enter German's age");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("enter Jess's age");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if ((number2 + number2) < 100)
+{ System.Console.WriteLine("You are still young"); }
+else
+{
+    System.Console.WriteLine("Get ready to die");
+}
